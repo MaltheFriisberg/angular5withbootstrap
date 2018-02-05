c23d596957d9e87aad108ddf04b103c9f87b178a
malthe <malthe@localhost.localdomain>
2018-02-05 22:31:37 +0100

    added server component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core/src/metadata/directives";
+
+//tell angular this is a component
+@Component({
+    //html tag selector
+    selector : 'app-server',
+    templateUrl : './server.component.html'
+})
+
+
+//ts class
+export class ServerComponent {
+
+}
